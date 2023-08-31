aaed8304e14cedef8552564ff222f81c2b1a56e1
Sanket <shindesanket497@gmail.com>
2023-08-31 17:26:11 +0530

    commit changes

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
-const strapi = require("@strapi/strapi");
-strapi(/* {...} */).start();
+const path = require('path');
+const serveStatic = require('serve-static');
+
+// ... Other Strapi configurations and middleware ...
+
+// Serve React build
+app.use(serveStatic(path.join(__dirname, 'public')));
+app.use('/uploads', serveStatic(path.join(__dirname, 'public/uploads')));
+
+// ... Other middleware and routing ...
+
+// Start the server
+strapi.start();
